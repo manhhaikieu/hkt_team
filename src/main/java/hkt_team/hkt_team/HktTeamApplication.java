b59ae48694b875a67d98327ebd54f7d01cbc16f1
manhhaikieu <manhhaimmiv@gmail.com>
2026-03-04 10:22:26 +0700

    init spring boot project

---
+++ src/main/java/hkt_team/hkt_team/HktTeamApplication.java
@@ -0,0 +1,13 @@
+package hkt_team.hkt_team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HktTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HktTeamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hkt_team/hkt_team/HktTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package hkt_team.hkt_team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HktTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
